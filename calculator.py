e4267cbe9b6126663e889bc656516bb857f507a7
FadingNinja <neetusdoshi@gmail.com>
2021-09-28 15:39:21 +0530

    simple calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter The First Number : "))
+num2 = float(input("Enter The Second Number : "))
+sum = num1 + num2
+minus = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+remainder = num1 % num2
+
+print(f"The Sum of Number 1 and Number 2 is : {sum}")
+print(f"The Subtraction of Number 1 and Number 2 is : {minus}")
+print(f"The Multiplication of Number 1 and Number 2 is : {multiply}")
+print(f"The Division of Number 1 and Number 2 is : {divide}")
+print(f"The Remainder of Number 1 and Number 2 is : {remainder}")
